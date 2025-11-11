e2661a4ed78da036a1957024b05332f472d704d8
yuba-k <160005925+yuba-k@users.noreply.github.com>
2025-11-11 23:32:49 +0900

    ログの追加

--- src/MaineController.py
+++ src/MaineController.py
@@ -2,12 +2,30 @@ import threading
 import queue
 import time
 
+import logging
+
 import motor
 import imgProcess
 import camera2
 
 frame_q = queue.Queue(maxsize=1)
 stop_event = threading.Event()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+filehandler = logging.FileHandler("cansat.log")
+filehandler.setLevel(logging.DEBUG)
+
+consolehandler = logging.StreamHandler()
+consolehandler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s-%(filename)s-%(funcName)s-%(levelname)s-%(message)s")
+filehandler.setFormatter(formatter)
+consolehandler.setFormatter(formatter)
+
+logger.addHandler(filehandler)
+logger.addHandler(consolehandler)
 
 def start_camera(picam):
     i = 0
@@ -36,14 +54,17 @@ def approach(mv):
         mv.adjust_duty_cycle(cmd)
 
 def main():
+    logger.info("CanSat起動")
     picam = camera2.Camera()
     mv = motor.Motor()
+    logger.info("初期化完了")
 
     mv.adjust_duty_cycle("stop")
     try:
         threading.Thread(target=mv.move, daemon=True).start()
         threading.Thread(target=start_camera, args=(picam,) ,daemon=True).start()
         threading.Thread(target=approach,args=(mv,), daemon=True).start()
+        logger.info("全スレッド起動")
         while not stop_event.is_set():
             time.sleep(1)
     finally:
@@ -51,6 +72,7 @@ def main():
         stop_event.set()
         picam.disconnect()
         mv.cleanup()
+        logger.info("正常に終了しました")
 
 if __name__ == "__main__":
     main()
