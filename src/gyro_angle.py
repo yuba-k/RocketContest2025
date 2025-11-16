961af7737c68878962207923a35e083f71bf518b
yuba-k <160005925+yuba-k@users.noreply.github.com>
2025-11-16 19:00:41 +0900

    6軸センサのデータ取得(Z軸のみ)

--- src/gyro_angle.py
+++ src/gyro_angle.py
@@ -1,5 +1,4 @@
 import time
-import math
 
 import board
 from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
@@ -7,35 +6,17 @@ from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
 i2c = board.I2C()
 sensor = LSM6DS33(i2c)
 
-print("3秒間の回転角度を計測します（X, Y, Z 軸）...")
+class GYRO():
+    def __init__(self):
+        self.gyro_z = 0.0
+        self._lasttime = None
 
-# 積分用の初期化
-angle_x = 0.0
-angle_y = 0.0
-angle_z = 0.0
-
-duration = 3.0  # 計測時間（秒）
-dt = 0.05       # サンプリング間隔（秒）
-samples = int(duration / dt)
-
-input("Enterで開始")#計測待機
-
-for i in range(samples):
-    gyro_x, gyro_y, gyro_z = sensor.gyro  # 単位: radians/s
-
-    # 積分（角速度 × 時間間隔）
-    angle_x += gyro_x * dt
-    angle_y += gyro_y * dt
-    angle_z += gyro_z * dt
-
-    time.sleep(dt)
-
-# ラジアン → 度 に変換
-angle_x_deg = math.degrees(angle_x)
-angle_y_deg = math.degrees(angle_y)
-angle_z_deg = math.degrees(angle_z)
-
-print(f"\n3秒間の回転角度:")
-print(f"X軸: {angle_x_deg:.2f}°")
-print(f"Y軸: {angle_y_deg:.2f}°")
-print(f"Z軸: {angle_z_deg:.2f}°")
+    def get_data(self):
+        if self._lasttime is None:
+            self._lasttime = time.perf_counter()
+            return 0.0
+        currenttime = time.perf_counter()
+        dt = currenttime - self._lasttime
+        row_z = sensor.gyro[2]
+        self.gyro_z += row_z * dt
+        return self.gyro_z
