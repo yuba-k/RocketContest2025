15b043c2f0b05f6e2962c3247fc5528035ff432f
yuba-k <160005925+yuba-k@users.noreply.github.com>
2025-12-24 13:18:36 +0900

    平行処理に変更

--- src/gyro_angle.py
+++ src/gyro_angle.py
@@ -1,32 +1,54 @@
 import time
 import math
+import threading
 
 import board
 from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
 
-i2c = board.I2C()
-sensor = LSM6DS33(i2c)
-
 
 class GYRO:
-    def __init__(self):
+    def __init__(self, sample_rate=0.01):
+        i2c = board.I2C()
+        self.sensor = LSM6DS33(i2c)
+
         self.gyro_z = 0.0
+        self.offset_z = 0.0
+        self.sample_rate = sample_rate
         self._lasttime = None
 
-    def get_data(self):
-        """
-        Returns:
-            float: 度数法で累積移動角を返値
-        """
-        if self._lasttime is None:
-            self._lasttime = time.perf_counter()
-            return 0.0
-        currenttime = time.perf_counter()
-        dt = currenttime - self._lasttime
-        row_z = sensor.gyro[2]
-        self.gyro_z += row_z * dt
-        return math.degrees(self.gyro_z)
+        self._calibrate()
 
+        self.thread = threading.Thread(target=self._update_loop, daemon=True)
+        self.thread.start()
+
+    def _calibrate(self, samples = 200):
+        total = 0
+        for _ in range(samples):
+            total += self.sensor.gyro[2]
+            time.sleep(0.005)
+        self.offset_z = total / samples
+
+    def _update_loop(self):
+        last_time = time.perf_counter()
+        while self.running:
+            current_time = time.pref_counter()
+            df = current_time - last_time
+
+            raw_z = self.sensor.gyro[2] - self.offset_z
+
+            if abs(raw_z) < 0.005:
+                raw_z = 0
+
+            self.gyro_z += raw_z * dict
+            
+            last_time = current_time
+            time.sleep(self.sample_rate)
+
+    def get_angle(self):
+        return self.gyro_z
+    
     def reset(self):
         self.gyro_z = 0.0
-        self._lasttime = None
+
+    def stop(self):
+        self.running = False
