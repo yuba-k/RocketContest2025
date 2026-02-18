626f0450eba32358460eb5fd48752be0e9b15d77
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-02-18 22:09:40 +0900

    ラズパイ初期起動コマンド

---
+++ raspi_init_command.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y vim
+sudo apt install -y python-opencv
+sudo apt install -y pip
+sudo apt install git
+git clone https://github.com/yuba-k/RocketContest2025.git
