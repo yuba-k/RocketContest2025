278e4346220913522545913f9367ff1e69988d30
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-02-28 22:22:45 +0900

    初期化コマンドの追加(pip3)

--- raspi_init_command.sh
+++ raspi_init_command.sh
@@ -6,3 +6,5 @@ sudo apt install -y python3-pip
 sudo apt install -y git
 sudo apt install -y libcamera-dev libcamera-apps
 git clone https://github.com/yuba-k/RocketContest2025.git
+cd RocketContest2025
+pip3 install -r requirements.txt --break-system-packages
