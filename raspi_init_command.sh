88b2b7c9a1c29d15481c32485843805f441bd0ae
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-03-02 15:23:51 +0900

    初期化コマンドの設定

--- raspi_init_command.sh
+++ raspi_init_command.sh
@@ -5,6 +5,12 @@ sudo apt install -y python3-opencv
 sudo apt install -y python3-pip
 sudo apt install -y git
 sudo apt install -y libcamera-dev libcamera-apps
+sudo apt install -y python3-libcamera
+sudo apt install -y python3-picamera2
 git clone https://github.com/yuba-k/RocketContest2025.git
 cd RocketContest2025
-pip3 install -r requirements.txt --break-system-packages
+pip3 install picamera2 -y --break-system-packages
+pip3 install board -y --break-system-packages
+pip3 install adafruit-circuitpython-lsm6ds -y --break-system-packages
+pip3 install pyserial -y --break-system-packages
+ pip3 install smbus -y --break-system-packages
